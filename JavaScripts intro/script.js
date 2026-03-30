bcdf7827dd6563185fea2a42a3a9eabe73782f1c
MRBeastGamerZz <eng22cs0101@dsu.edu.in>
2026-03-30 14:38:55 +0530

    js initial

---
+++ JavaScripts intro/script.js
@@ -0,0 +1,33 @@
+// alert("hello world!")
+console.log("Hey this is JS tutorial")
+
+let a=5;
+var b=6;
+var c="JavaScripts";
+
+console.log(a + b + 8)
+console.log(typeof a, typeof b, typeof c)
+
+{
+    let a=55;
+    console.log(a)
+}
+console.log(a)
+
+let x = "john";
+let y = "555";
+let z = "82.456";
+const r = true;
+let p = undefined;
+let q = null;
+
+console.log(x, y, z, r, p, q)
+console.log(typeof x, typeof y, typeof z, typeof r, typeof p, typeof q)
+
+let o ={
+    "name": "Harry",
+    "job code":556600
+}
+console.log(o)
+o.salary ="1 lakh"
+console.log(o)
---
+++ JavaScripts intro/script1.js
@@ -0,0 +1,78 @@
+console.log("Conditional tutorial in JS")
+
+// let age =11;
+// let g=8;
+
+// if(age+g>18){
+//     console.log("you can drive");
+// }
+// else{
+//     console.log("you can not drive");
+// }
+
+
+
+
+console.log("Loops tutorial in JS")
+
+// let a= 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+    
+// }
+
+// let obj={
+//     name:"Harry",
+//     role:"programmer",
+//     company:"CodeWithHarry"
+// }
+
+// for (const key in obj) {
+//     console.log(key)
+// }
+
+// for (const c of "Harry") {
+//     console.log(c)
+// }
+
+// let i=10;
+// // while (i<6) {
+// //     console.log(i);
+// //     i++;
+// // }
+
+// do {
+//     console.log(i)
+//     i++
+// } while (i<5);
+
+
+
+
+console.log("Functions tutorial in JS")
+
+function nice(name) {
+    console.log("Hey "+name+" your are nice")
+    console.log("Hey "+name+" your are good")
+    console.log("Hey "+name+" are you fine!")
+    console.log("Hey "+name+" your shirt is good!")
+}
+// nice("John")
+// nice("Harry")
+
+function sum(a ,b) {
+    // console.log(a+b)
+    return a+b
+    
+}
+// sum(2,3)
+// sum(5,3)
+// result=sum(5,5)
+// console.log("the sum is: ", result)
+
+
+
+
+
+
+
